e39af799538204fb0538948fd00e59e8df95e362
Hanna <102633359+hannaalfi@users.noreply.github.com>
2022-11-01 22:07:30 -0700

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,34 +1,123 @@
 #!/usr/bin/python3
-"""
-This module defines the "Base" class
-"""
-
+'''
+Almost a circle project
+ALX program
+'''
 import json
-
+import csv
 
 class Base:
-    """Base class"""
+    '''
+    Base class of the project
+    '''
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initializes Base"""
-        if id is None:
-            Base.__nb_objects += 1
-            self.id = self.__nb_objects
-        else:
+        '''
+        Instantiatior
+        '''
+        if id is not None:
             self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """returns the JSON string representation of a list of dictionaries"""
-        if list_dictionaries is None or list_dictionaries == []:
-            list_dictionaries = []
-        return json.dumps(list_dictionaries)
+        '''
+        returns the JSON string representation
+        of list_dictionaries
+        '''
+        if list_dictionaries and len(list_dictionaries) > 0:
+            return json.dumps(list_dictionaries)
+        else:
+            return "[]"
 
     @staticmethod
     def from_json_string(json_string):
-        """returns the list of the JSON string representation json_string"""
-        if json_string is None or len(json_string) == 0:
-            return []
-        return json.loads(json_string)
-Footer
+        '''
+        returns the list of the JSON
+        string representation json_string
+        '''
+        if json_string and json_string != '':
+            return json.loads(json_string)
+        else:
+            return list()
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''
+        writes the JSON string representation
+        of list_objs to a file
+        '''
+        new_list = []
+        if list_objs and len(list_objs) > 0:
+            str = ''
+            for item in list_objs:
+                new_list.append(item.to_dictionary())
+        with open(cls.__name__ + '.json', 'w') as f:
+            f.write(Base.to_json_string(new_list))
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''
+        Returns an instance with all attributes
+        already set according to dictionary
+        '''
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        '''
+        Return a list of instances
+        '''
+        new_list = list()
+        try:
+            with open(cls.__name__ + '.json', 'r') as f:
+                file_string = f.read()
+                ins_list = Base.from_json_string(file_string)
+                for item in ins_list:
+                    new_list.append(cls.create(**item))
+                return new_list
+        except Exception:
+            return new_list
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        '''
+        serializes in CSV
+        '''
+        new_list = []
+        with open(cls.__name__ + '.csv', 'w') as f:
+            if list_objs and len(list_objs) > 0:
+                if cls.__name__ == 'Rectangle':
+                    header = ['id', 'width', 'height', 'x', 'y']
+                elif cls.__name__ == 'Square':
+                    header = ['id', 'size', 'x', 'y']
+
+                writer = csv.DictWriter(f, header)
+                writer.writeheader()
+                for item in list_objs:
+                    writer.writerow(item.to_dictionary())
+            else:
+                file.write('[]')
+
+    @classmethod
+    def load_from_file_csv(cls):
+        '''
+        deserializes from CSV
+        '''
+        new_list = list()
+        try:
+            with open(cls.__name__ + '.csv', 'r', newline='') as csvf:
+                freader = csv.reader(csvf)
+                for row in freader:
+                    new_list.append(cls.create(**row))
+                return new_list
+        except Exception:
+            return 
